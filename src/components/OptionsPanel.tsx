255284673ca013bdfe587fd87684cea898eba61e
LukeBatten17 <lukebatten17@gmail.com>
2025-12-13 11:46:34 -0330

    Added 1 as an option for # of rounds

--- src/components/OptionsPanel.tsx
+++ src/components/OptionsPanel.tsx
@@ -36,6 +36,7 @@ const OptionsPanel: React.FC<OptionsPanelProps> = ({
           className="bg-background"
           ariaLabel="number of rounds"
         >
+          <option value={1}>1</option>
           <option value={5}>5</option>
           <option value={10}>10</option>
           <option value={25}>25</option>
